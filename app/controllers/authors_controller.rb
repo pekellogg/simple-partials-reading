8f7692559d393ce92518c98513c870c7f92b6684
Peyton Kellogg <pekellogg@hotmail.com>
2022-05-23 16:51:08 -0700

    passing specs

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,13 @@
 class AuthorsController < ApplicationController
+  # "authors"
+  #   t.string   "name"
+  #   t.string   "hometown"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+
+  # routes
+  # resources :authors, only: :show
+  
   def show
     @author = Author.find(params[:id])
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,21 @@
 class PostsController < ApplicationController
+  # posts
+  #   t.string   "title"
+  #   t.text     "description"
+  #   t.datetime "created_at",  null: false
+  #   t.datetime "updated_at",  null: false
+  #   t.integer  "author_id"
+  
+  # routes
+  # resources :posts
+
   def index
     @posts = Post.all
   end
 
   def show
     @post = Post.find(params[:id])
+    @author = @post.author
   end
 
   def new
